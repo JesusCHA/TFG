81367b876f9a528c31cb22f9996c8428e7a48d10
JesusCHA <jechaveza@alumnos.unex.es>
2017-09-27 11:44:56 +0200

    Main con semilla

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,9 @@
 
 #include "calculos.h"
 #include "filtrado_hv.h"
+#include <time.h>
 
-#define num_rep 31
+#define num_rep 31 // -- 31
 
 int main(void) {
 	
@@ -24,6 +25,9 @@ int main(void) {
 	if(remove("All_HV.csv")==0) // Eliminamos el archivo
 
 	for (int rep=0; rep < num_rep; rep++){
+			
+		srand(time(NULL)+rep);
+
 		if(rep < 10)
 			nFichero = "sol0"+std::to_string(rep);
 		else{ 
